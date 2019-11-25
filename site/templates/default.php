31aec164465054613d78b82b262a6cda360c5767
Samuel <samuelvvelzen@gmail.com>
2019-11-25 08:58:16 +0100

    changed default to show new content and references to assets

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,9 +6,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?= css([
+        'assets/css/main.css',
+        'assets/css/owl.carousel.min.css',
+    ]) ?>
+
+    <?= js('assets/js/main.js') ?>
 
     <title><?= $page->title() ?></title>
 </head>
@@ -16,19 +19,100 @@
 <body>
     <h1><?= $page->title() ?></h1>
 
+    <div class="owl-carousel">
+        <img src="<?= image('placeholder.jpg')->url() ?>" alt="" />
+        <img class=" middle" src="<?= image('placeholder.jpg')->url() ?>" alt="" />
+        <img class="middle" src="<?= image('placeholder.jpg')->url() ?>" alt="" />
+        <img class="middle" src="<?= image('placeholder.jpg')->url() ?>" alt="" />
+        <img class="middle" src="<?= image('placeholder.jpg')->url() ?>" alt="" />
+        <img class="middle" src="<?= image('placeholder.jpg')->url() ?>" alt="" />
 
-    <p><?= $page->inleiding()->kirbytext() ?></p>
+        <img src="<?= image('placeholder.jpg')->url() ?>" alt="" />
+    </div>
 
-    <!-- Optional JavaScript -->
+
+    <h1 class="titel-inleiding">Inleiding</h1>
+    <p class="Inleiding">
+        <?= $page->inleiding()->kirbytext() ?>
+    </p>
+
+    <div class="cardscontainer">
+        <div class="card">
+            <h1>Waarom</h1>
+            <div class="card-content">
+                <div class="placeholder">x</div>
+                <p class="text">
+                    <?= $page->why_inleiding()->kirbytext() ?>
+                </p>
+                <button class="actionbutton" onclick="selectSubject.showContent('why')">
+                    Verder
+                </button>
+            </div>
+        </div>
+
+        <div class="card">
+            <h1>Hoe</h1>
+            <div class="card-content">
+                <div class="placeholder">x</div>
+                <p class="text">
+                    <?= $page->how_inleiding()->kirbytext() ?>
+                </p>
+                <button class="actionbutton" onclick="selectSubject.showContent('how')">
+                    Verder
+                </button>
+            </div>
+        </div>
+
+        <div class="card">
+            <h1>Wat</h1>
+            <div class="card-content">
+                <div class="placeholder">x</div>
+                <p class="text">
+                    <?= $page->what_inleiding()->kirbytext() ?>
+                </p>
+                <button class="actionbutton" onclick="selectSubject.showContent('what')">
+                    Verder
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="content-header">
+            <p id="whyChoose">Waarom</p>
+            <p id="howChoose">Hoe</p>
+            <p id="whatChoose">Wat</p>
+        </div>
+
+        <div class="content-body">
+            <div id="whyContent" class="content-body-section">
+                <?= $page->why_content()->kirbytext() ?>
+            </div>
+            <div id="howContent" class="content-body-section">
+                <?= $page->how_content()->kirbytext() ?>
+            </div>
+            <div id="whatContent" class="content-body-section">
+                <?= $page->what_content()->kirbytext() ?>
+            </div>
+        </div>
+    </div>
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+
+    <!-- Optional JavaScript -->
+    <?= js('assets/js/owl.carousel.min.js') ?>
+
+    <script>
+    $(document).ready(function() {
+        $(".owl-carousel").owlCarousel({
+            items: 2
+        });
+    });
+
+    selectSubject.init();
     </script>
 </body>
 
