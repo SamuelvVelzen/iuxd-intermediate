fd06137351cecd643f2b004ec9d04b5264e7f652
Samuel <samuelvvelzen@gmail.com>
2020-01-12 17:30:49 +0100

    improving whw and added hidden content

--- site/templates/default.php
+++ site/templates/default.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://use.typekit.net/gmx3xsc.css">
-    
+
     <?= css([
         'assets/css/main.css',
     ]) ?>
@@ -52,88 +52,66 @@
             <h1 class="title">Onderwerpen</h1>
 
             <div class="what question">
-            <h1 class='title'>Wat</h1>
                 <div class="text">
-                    <?= $page->Why_inleiding() ?>
+                    <h1 class='title'>Wat</h1>
+                    <p><?= $page->What_inleiding() ?></p>
+
+                    <button id="what" class="button">Lees meer</button>
+
+                    <div id="whatContent" class="hiddenContent">
+                        <p><?= $page->What_content() ?></p>
+                    </div>
                 </div>
                 <?php if($image = $page->image('mock-up-wat.png')): ?>
-                    <img src="<?= $image->url() ?>" alt=""class='image'>
+                <img src="<?= $image->url() ?>" alt="" class='image'>
                 <?php endif ?>
             </div>
+
+            <hr>
+
             <div class="waarom question">
-                <div class="image"></div>
-                <h1 class='title'>Waarom</h1>
                 <div class="text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
+                    <h1 class='title'>Waarom</h1>
+                    <p><?= $page->Why_inleiding() ?></p>
 
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
+                    <button id="why" class="button">Lees meer</button>
 
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
+                    <div id="whyContent" class="hiddenContent">
+                        <p><?= $page->Why_content() ?></p>
+                    </div>
                 </div>
+                <?php if($image = $page->image('workspace-mockup-waarom.png')): ?>
+                <img src="<?= $image->url() ?>" alt="" class='image'>
+                <?php endif ?>
             </div>
+            </div>
+
+            <hr>
+
             <div class="Hoe question">
-            <h1 class='title'>Hoe - Stappenplan</h1>
                 <div class="text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
+                    <h1 class='title'>Hoe - Stappenplan</h1>
+                    <p><?= $page->How_inleiding() ?></p>
 
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
+                    <button id="how" class="button">Lees meer</button>
 
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-                    laborum corporis quibusdam in ea soluta inventore! Molestiae autem
-                    voluptatibus doloribus repellat cupiditate laborum omnis
-                    necessitatibus, assumenda soluta aspernatur, voluptate voluptatem.
+                    <div id="howContent" class="hiddenContent">
+                        <p><?= $page->how_content() ?></p>
+                    </div>
                 </div>
-                <div class="image"></div>
+                <?php if($image = $page->image('mock-up-wat.png')): ?>
+                <img src="<?= $image->url() ?>" alt="" class='image'>
+                <?php endif ?>
+            </div>
             </div>
         </section>
         <section id="steps" class="stepscontainer">
-          
+
         </section>
+
+        <script>
+        main.init();
+        </script>
     </body>
 </body>
 
