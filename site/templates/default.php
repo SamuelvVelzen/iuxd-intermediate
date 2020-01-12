17a2ca7f39fe9ed3a42e824224951f0e091e30b4
Gabrielle <gabriellevdvalk@live.nl>
2020-01-12 17:00:46 +0100

    fonts

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,6 +6,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="https://use.typekit.net/gmx3xsc.css">
+    
     <?= css([
         'assets/css/main.css',
     ]) ?>
