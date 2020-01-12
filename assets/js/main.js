6c1e5332e42cf98c8bc33eff5cac547af2a8cbfd
Samuel <samuelvvelzen@gmail.com>
2020-01-12 17:30:58 +0100

    added logic for hidden content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,105 +1,35 @@
-var selectSubject = (function() {
+var main = (function() {
   //variables
   var uiStrings = {
-    chooseW: "whyChoose",
-    contentW: "whyContent",
-    chooseH: "howChoose",
-    contentH: "howContent",
-    chooseT: "whatChoose",
-    contentT: "whatContent"
+    chooseT: "what",
+    chooseW: "why",
+    chooseH: "how"
   };
 
   //functions
-  var addEvents, showContent;
+  var addEvents, _checkContent;
 
-  showContent = function(type) {
-    var elW = document.getElementById(uiStrings.chooseW),
-      elH = document.getElementById(uiStrings.chooseH),
-      elT = document.getElementById(uiStrings.chooseT),
-      contentW = document.getElementById(uiStrings.contentW),
-      contentH = document.getElementById(uiStrings.contentH),
-      contentT = document.getElementById(uiStrings.contentT);
+  _checkContent = function(el) {
+    var content = document.getElementById(el.id + "Content");
 
-    if (type == "why") {
-      if (!contentW.classList.contains("active")) {
-        //change active selectors
-        elW.classList.add("active");
-
-        contentH.classList.add("flyout");
-        contentT.classList.add("flyout");
-
-        setTimeout(() => {
-          contentH.classList.remove("flyout");
-          contentT.classList.remove("flyout");
-          contentW.classList.add("active");
-        }, 500);
-      }
-      //remove active selectors
-      elH.classList.remove("active");
-      elT.classList.remove("active");
-
-      //change content
-      contentH.classList.remove("active");
-      contentT.classList.remove("active");
-    } else if (type == "how") {
-      if (!contentH.classList.contains("active")) {
-        //change active selectors
-        elH.classList.add("active");
-
-        contentW.classList.add("flyout");
-        contentT.classList.add("flyout");
-
-        setTimeout(() => {
-          contentW.classList.remove("flyout");
-          contentT.classList.remove("flyout");
-          contentH.classList.add("active");
-        }, 500);
-      }
-      //remove active selectors
-      elW.classList.remove("active");
-      elT.classList.remove("active");
-
-      //change content
-      contentW.classList.remove("active");
-      contentT.classList.remove("active");
+    if (content.classList.contains("open")) {
+      content.classList.remove("open");
     } else {
-      //what
-
-      if (!contentT.classList.contains("active")) {
-        //change active selectors
-        elT.classList.add("active");
-
-        contentH.classList.add("flyout");
-        contentW.classList.add("flyout");
-
-        setTimeout(() => {
-          contentH.classList.remove("flyout");
-          contentW.classList.remove("flyout");
-          contentT.classList.add("active");
-        }, 500);
-      }
-      //remove active selectors
-      elH.classList.remove("active");
-      elW.classList.remove("active");
-
-      //change content
-      contentH.classList.remove("active");
-      contentW.classList.remove("active");
+      content.classList.add("open");
     }
   };
 
   addEvents = function() {
-    var elW = document.getElementById(uiStrings.chooseW),
-      elH = document.getElementById(uiStrings.chooseH),
-      elT = document.getElementById(uiStrings.chooseT);
+    var elT = document.getElementById(uiStrings.chooseT),
+      elW = document.getElementById(uiStrings.chooseW),
+      elH = document.getElementById(uiStrings.chooseH);
 
-    elW.addEventListener("click", () => showContent("why"));
-    elH.addEventListener("click", () => showContent("how"));
-    elT.addEventListener("click", () => showContent("what"));
+    elT.addEventListener("click", () => _checkContent(elT));
+    elW.addEventListener("click", () => _checkContent(elW));
+    elH.addEventListener("click", () => _checkContent(elH));
   };
 
   return {
-    init: addEvents,
-    showContent: showContent
+    init: addEvents
   };
 })();
