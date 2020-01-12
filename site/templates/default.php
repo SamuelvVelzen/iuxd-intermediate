f785680c860d06b2d6d19bde99a3f358c1c54c90
Samuel <samuelvvelzen@gmail.com>
2020-01-12 20:20:11 +0100

    make button clickable, added reviews section

--- site/templates/default.php
+++ site/templates/default.php
@@ -44,12 +44,12 @@
                 <p class="introductie">
                     <?= $page->Inleiding() ?>
                 </p>
-                <button>Lees meer</button>
+                <a class="btn clickableNav" href="#whwTitle">Lees meer</a>
             </div>
 
         </section>
         <section id="whw">
-            <h1 class="title">Onderwerpen</h1>
+            <h1 id="whwTitle" class="title">Onderwerpen</h1>
 
             <div class="what question">
                 <div class="text">
@@ -109,48 +109,88 @@
             </div>
             </div>
         </section>
-        <section id="steps" >
-        <h1 class="title">Stappenplan</h1>
-        <div class='stepscontainer'>
-            <div class="step">
-                <h1>1</h1>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
-                    unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
-                    enim minima incidunt nemo sunt necessitatibus voluptatum?
-                    Necessitatibus quo sequi eligendi?
-                </p>
-            </div>
+        <section id="steps">
+            <h1 class="title">Stappenplan</h1>
+            <div class='stepscontainer'>
+                <div class="step">
+                    <h1>1</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
+                        unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
+                        enim minima incidunt nemo sunt necessitatibus voluptatum?
+                        Necessitatibus quo sequi eligendi?
+                    </p>
+                </div>
 
-            <div class="step">
-                <h1>2</h1>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
-                    unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
-                    enim minima incidunt nemo sunt necessitatibus voluptatum?
-                    Necessitatibus quo sequi eligendi?
-                </p>
-            </div>
+                <div class="step">
+                    <h1>2</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
+                        unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
+                        enim minima incidunt nemo sunt necessitatibus voluptatum?
+                        Necessitatibus quo sequi eligendi?
+                    </p>
+                </div>
 
-            <div class="step">
-                <h1>3</h1>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
-                    unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
-                    enim minima incidunt nemo sunt necessitatibus voluptatum?
-                    Necessitatibus quo sequi eligendi?
-                </p>
-            </div>
+                <div class="step">
+                    <h1>3</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
+                        unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
+                        enim minima incidunt nemo sunt necessitatibus voluptatum?
+                        Necessitatibus quo sequi eligendi?
+                    </p>
+                </div>
 
-            <div class="step">
-                <h1>4</h1>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
-                    unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
-                    enim minima incidunt nemo sunt necessitatibus voluptatum?
-                    Necessitatibus quo sequi eligendi?
-                </p>
-            </div></div>
+                <div class="step">
+                    <h1>4</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
+                        unde molestiae, expedita ea perspiciatis blanditiis vitae debitis rem
+                        enim minima incidunt nemo sunt necessitatibus voluptatum?
+                        Necessitatibus quo sequi eligendi?
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section id="workshop">
+            <h1 class="title">Wat onze workshop deelnemers zeggen</h1>
+
+            <div class="reviewsgroup">
+                <div class="review">
+                    <p class="quote">
+                        "Hele goede workshop. Heel veel informatie, maar iets teveel om ik korte tijd tot je
+                        te nemen. Volgende keer misschien tijdens de workshop iets meer nadruk leggen op een klein
+                        gedeelte van de presentatie in plaats van het hele proces te moeten doorlopen"
+                    </p>
+                    <div class="info">
+                        <span class="picture"></span>
+                        <div class="personal">
+                            <p class="name">Dimitri Philips</p>
+                            <p class="work">
+                                Klasgenote minor
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="review">
+                    <p class="quote">
+                        "Heel leuk om te zien hoe branding binnen een bedrijf werkt en hoe je dit kunt
+                        toepassen."
+                    </p>
+                    <div class="info">
+                        <span class="picture"></span>
+                        <div class="personal">
+                            <p class="name">Dionne de Vries</p>
+                            <p class="work">
+                                Beste vriendin Gabriel
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <script>
